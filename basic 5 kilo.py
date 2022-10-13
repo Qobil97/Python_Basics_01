1c2e78d6c27674c798180dede71786338a409cdd
Qobil97 <kobiljon.kahramonov@gmail.com>
2022-10-13 18:38:42 +0200

    init

---
+++ basic 5 kilo.py
@@ -0,0 +1,6 @@
+user_name=input("What is your name? ")
+print("Hi "+user_name)
+
+weight_in_pounds=input("What is your wieght in pounds? ")
+weight_in_kilos=int(weight_in_pounds) * 0.45
+print(format(weight_in_kilos)+" kilos")
